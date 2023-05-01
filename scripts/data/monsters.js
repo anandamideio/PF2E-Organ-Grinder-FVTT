9c86aa8dc436ad3a37ea421c89da2dc13a4f0a8f
Abourass <Abourassa@AssetVal.com>
2023-04-30 23:58:33 -0600

    :art: More

---
+++ scripts/data/monsters.js
@@ -0,0 +1,5 @@
+export const monsters = [
+    { name: 'Slurk', traits: ['animal'], additionalTraits: ['reptile', 'frog', 'tusked'] },
+    { name: 'Giant Fly', traits: ['animal'], additionalTraits: ['insect', 'fly', 'winged'] },
+];
+export default monsters;
